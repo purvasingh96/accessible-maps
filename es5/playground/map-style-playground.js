6c35248348a65b8bea454dcc64ceff269e2c8920
psingh359 <singhpurva2906@gmail.com>
2022-11-07 12:22:37 -0500

    Updates

---
+++ es5/playground/map-style-playground.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.dataLayer = void 0;
+// For more information on data-driven styles, see https://www.mapbox.com/help/gl-dds-ref/
+exports.dataLayer = {
+    id: 'data',
+    type: 'fill',
+    paint: {
+        'fill-color': 'red',
+        'fill-opacity': 0.5
+    }
+};
+//# sourceMappingURL=map-style-playground.js.map
