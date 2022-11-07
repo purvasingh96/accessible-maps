6a42516ecfc3520f13229f1a1561608c775f48d7
psingh359 <singhpurva2906@gmail.com>
2022-11-07 13:06:36 -0500

    Updates

--- esm/heatmap/heatmap-control-panel.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-declare type GeoMapControlPanelProps = {
-    name: String;
-    dimensions: Map<string, string>;
-    onChange: Function;
-};
-declare function GeoMapControlPanel(props: GeoMapControlPanelProps): JSX.Element;
-export default GeoMapControlPanel;
